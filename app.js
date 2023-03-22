dbdb6d8295abd1c34aebd2dc0bb2242b205e2d17
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-03-22 08:34:00 +0100

    south park

---
+++ app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const kyle = {
+  name: "Kyle Broflovski",
+  nickname: "Daywalker",
+  image: "https://upload.wikimedia.org/wikipedia/en/2/25/KyleBroflovski.png",
+  occupation: "student",
+  age: "10",
+  voicedBy: "Matt Stone",
+  gender: "male",
+  religion: "Judaism",
+  catchPhrase: "God Dammit Cartman!",
+  hairColor: "red",
+  schoolGrade: 4,
+  episodes: "??",
+  appearances: "??",
+  firstAppearance: "S1, E1",
+};
