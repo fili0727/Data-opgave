1f5d852521a966c5ac3286eaca039679ea8b2e01
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-03-23 11:53:36 +0100

    grid

--- app.js
+++ app.js
@@ -1,5 +1,47 @@
 "use strict";
 
+window.addEventListener("load", start);
+
+function start() {
+  const data = getData();
+  showAllCharacters(data);
+}
+
+function getData() {
+  const data = [];
+  return data;
+}
+
+function showAllCharacters(list) {
+  showCharacter(kyle);
+  showCharacter(kyle);
+  showCharacter(kyle);
+}
+
+function showCharacter(character) {
+  const html = /*html*/ `
+            <article class="grid-item">
+                <img src="${character.image}">
+                <h2>${character.name}</h2>
+                <p>${character.nickname}</p>
+                <p>Age: ${character.age}</p>
+                <p>Voiced by ${character.voicedBy}</p>
+            </article>
+        `;
+  document.querySelector("#characters").insertAdjacentHTML("beforeend", html);
+}
+
+function showDialog(charatcer) {}
+
+function closeDialog() {}
+
+function getDescription() {}
+
+function formatDate() {}
+
+function calcAge() {}
+
+//temporary
 const kyle = {
   name: "Kyle Broflovski",
   nickname: "Daywalker",
@@ -12,7 +54,7 @@ const kyle = {
   catchPhrase: "God Dammit Cartman!",
   hairColor: "red",
   schoolGrade: 4,
-  episodes: "??",
-  appearances: "??",
-  firstAppearance: "S1, E1",
+  episodes: null,
+  appearances: undefined,
+  firstAppearance: "S01E01",
 };
