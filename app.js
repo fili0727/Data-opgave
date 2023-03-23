44c8f976cc638044f1f216630d8211d77c154540
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-03-23 14:00:21 +0100

    json

--- app.js
+++ app.js
@@ -2,33 +2,72 @@
 
 window.addEventListener("load", start);
 
-function start() {
-  const data = getData();
-  showAllCharacters(data);
+async function start() {
+  // const data = getData();
+  const kyle = await getData("kyle.json");
+  //   showAllCharacters();
+  showCharacter(kyle);
+  showCharacter(kyle);
+  showCharacter(kyle);
 }
 
-function getData() {
-  const data = [];
+async function getData(url) {
+  const response = await fetch(url); //fetch request returning a response
+  const data = await response.json(); //parsing data from json to JavaScript
   return data;
 }
 
-function showAllCharacters(list) {
-  showCharacter(kyle);
-  showCharacter(kyle);
-  showCharacter(kyle);
-}
+// function showAllCharacters(character) {
+//   showCharacter(kyle);
+// }
 
 function showCharacter(character) {
   const html = /*html*/ `
             <article class="grid-item">
                 <img src="${character.image}">
                 <h2>${character.name}</h2>
-                <p>${character.nickname}</p>
+                <p>nickname: ${character.nickname}</p>
                 <p>Age: ${character.age}</p>
                 <p>Voiced by ${character.voicedBy}</p>
             </article>
         `;
   document.querySelector("#characters").insertAdjacentHTML("beforeend", html);
+  document
+    .querySelector("#characters article:last-child")
+    .addEventListener("click", characterClicked);
+}
+
+function characterClicked(character) {
+  showCharacterModal(character);
+}
+
+function showCharacterModal(character) {
+  console.log(character);
+  document.querySelector("#dialog_image").src = character.image;
+  document.querySelector("#dialog_title").textContent = character.name;
+  document.querySelector("#dialog_religion").textContent = character.religion;
+  let description = generateDescription(character);
+  document.querySelector("#dialog_character_description").textContent =
+    description;
+
+  document.querySelector("#dialog_gender").textContent = character.gender;
+  document.querySelector("#dialog_hair_color").textContent =
+    character.hairColor;
+  document.querySelector("#dialog_occupation").textContent =
+    character.occupation;
+  document.querySelector("#dialog_catchphrase").textContent =
+    character.catchPhrase;
+  document.querySelector("#dialog_appearances").textContent =
+    character.appearances;
+  document.querySelector("#dialog_first_appearance").textContent =
+    character.firstAppearance;
+
+  document.querySelector("#dialog_name").textContent = character.name;
+  document.querySelector("#dialog_voiced_by_name").textContent =
+    character.voicedBy;
+
+  // show dialog
+  document.querySelector("#dialog_character").showModal();
 }
 
 function showDialog(charatcer) {}
@@ -40,21 +79,3 @@ function getDescription() {}
 function formatDate() {}
 
 function calcAge() {}
-
-//temporary
-const kyle = {
-  name: "Kyle Broflovski",
-  nickname: "Daywalker",
-  image: "https://upload.wikimedia.org/wikipedia/en/2/25/KyleBroflovski.png",
-  occupation: "student",
-  age: "10",
-  voicedBy: "Matt Stone",
-  gender: "male",
-  religion: "Judaism",
-  catchPhrase: "God Dammit Cartman!",
-  hairColor: "red",
-  schoolGrade: 4,
-  episodes: null,
-  appearances: undefined,
-  firstAppearance: "S01E01",
-};
